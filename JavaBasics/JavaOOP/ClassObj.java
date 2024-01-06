288c793b406c8e6a1629221dcaf4ba98f7369e4a
soham70 <shethsoham2001@gmail.com>
2024-01-06 12:01:40 +0530

    first commit

---
+++ JavaBasics/JavaOOP/ClassObj.java
@@ -0,0 +1,17 @@
+package JavaOOP;
+
+
+public class ClassObj {
+    int x=10,y=20;
+
+    public static void main(String[] args)
+    {
+        ClassObj myObj=new ClassObj();
+        ClassObj obj=new ClassObj();
+        
+        System.out.println(myObj.x);
+        System.out.println(obj.y);
+    }
+    
+}
+
