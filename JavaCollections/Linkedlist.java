f06efcdc57794095f1de08cb26342de1d98ce8b0
soham70 <shethsoham2001@gmail.com>
2024-01-17 16:04:55 +0530

    initial commit

--- JavaCollections/Linkedlist.java
+++ JavaCollections/Linkedlist.java
@@ -1,4 +1,47 @@
 package JavaCollections;
+
+import java.util.LinkedList;
+import java.util.Collections;
+
+
 public class Linkedlist {
-    
+    public static void main(String[] args)
+    {
+        LinkedList<Integer> num =new LinkedList<Integer>();
+        num.add(20);
+        num.add(50);
+        num.add(70);
+        num.add(10);
+        num.add(30);
+        num.add(90);
+        System.out.println(num);
+
+        num.remove(2);
+
+        System.out.println(num.removeFirst());
+        System.out.println(num.removeLast());
+        System.out.println(num.getFirst());
+        System.out.println(num.getLast());
+        num.addFirst(60);
+        num.addLast(100);
+
+        System.out.println(num);
+
+        Collections.sort(num);
+
+        System.out.println(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
