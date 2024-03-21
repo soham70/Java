72d12050b505a315589de6e46b3e5208378321a5
soham70 <shethsoham2001@gmail.com>
2024-03-22 00:24:50 +0530

    initual commit

--- intermediate/src/Hashset_Program.java
+++ intermediate/src/Hashset_Program.java
@@ -4,5 +4,26 @@ public class Hashset_Program {
     public static void main(String[] args)
     {
         HashSet<String>sports = new HashSet<>();
+
+        sports.add("Volleyball");
+        sports.add("Cricket");
+        sports.add("Badminton");
+        sports.add("Hockey");
+        sports.add("Baseball");
+
+        System.out.println(sports);
+        System.out.println(sports.contains("Cricket"));
+        sports.remove("Hockey");
+        System.out.println(sports);
+//        sports.clear();
+//        System.out.println(sports);'
+
+        System.out.println(sports.size());
+
+        for (String i:sports)
+        {
+            System.out.println(i);
+        }
+
     }
 }
